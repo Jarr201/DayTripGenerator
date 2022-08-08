babbb261b77bd820d938b4c8242d9b80ead2ca0e
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-08 13:35:03 -0400

    Added the full trip dialogue prompt to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Github testing")
+destination = ["Bahamas", "Tokyo", "New York", "Florida"]
+restuarant = ["fast food place", "buffet", "diner", "bar"]
+transportation = ["train", "airplane", "car", "cruise ship"]
+entertainment  = ["nature walk", "shopping spree", "city tour", "beach visit"]
+
+intro = input("Hello and Welcome to your trip planner! Lets get you started!")
+print(intro)
+
+full_trip = f"You'll be visiting {destination} via {transportation}. During your trip, you should eat at a {restuarant} and go on a {entertainment} while you're out!"
+print(full_trip)
