558a7bfc04966523032e8ee057c4d2c5fe6daff1
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-08 15:24:49 -0400

    Converted the intro section, trip components, and confirmations into functions in main.py

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import random
 
-destination = ["the Bahamas", "Tokyo", "New York", "Florida"]
+destination = ["The Bahamas", "Tokyo", "New York", "Florida"]
 restuarant = ["fast food place", "buffet", "diner", "bar"]
 transportation = ["train", "airplane", "car", "cruise ship"]
 entertainment  = ["nature walk", "shopping spree", "city tour", "beach visit"]
 
-intro = "Hello and Welcome to your trip planner! Lets get you started!"
-print(intro)
+def begin(intro = "Hello and Welcome to your trip planner! Lets get you started!"):
+    print(intro)
 
-trip_dest = random.choice(destination)
-trip_rest = random.choice(restuarant)
-trip_trans = random.choice(transportation)
-trip_enter = random.choice(entertainment)
+begin()
 
+def Trip_component():
+    trip_dest = random.choice(destination)
+    trip_rest = random.choice(restuarant)
+    trip_trans = random.choice(transportation)
+    trip_enter = random.choice(entertainment)
+    full_trip = f"You'll be visiting {trip_dest} via {trip_trans}. During your trip, you should eat at a {trip_rest} and go on a {trip_enter} while you're out!"
+    print(full_trip)
+    return
 
-full_trip = f"You'll be visiting {trip_dest} via {trip_trans}. During your trip, you should eat at a {trip_rest} and go on a {trip_enter} while you're out!"
-print(full_trip)
+Trip_component()
 
-inquiry = input("Do you like your trip? Y/N ")
-
-if inquiry == "Y":
-    print("We're glad you're satisfied! Have a great trip!")
-elif inquiry == "N":
-    print("No problem! Let's make this plan work for you.")
-else:
-    print(input("Sorry. Please answer again in the correct format. Y/N "))
-    
+def confirmation():
+    inquiry = input("Do you like your trip? Y/N ")
+    if inquiry == "Y":
+        print("We're glad you're satisfied! Have a great trip!")
+        print("Program Complete!")
+    elif inquiry == "N":
+        print("No problem! Let's make this plan work for you.")
+    else:
+        print(input("Sorry. Please answer again in the correct format. Y/N "))
+    return inquiry
+ 
+confirmation()
