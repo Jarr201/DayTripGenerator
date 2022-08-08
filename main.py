1d2652a2b393a0c926d41530daedee0f822f936c
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-08 13:45:47 -0400

    Added the import random module and assigned variables to the randomizer functions. Also created the fully randomized dialogue.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 destination = ["Bahamas", "Tokyo", "New York", "Florida"]
 restuarant = ["fast food place", "buffet", "diner", "bar"]
 transportation = ["train", "airplane", "car", "cruise ship"]
@@ -6,5 +8,11 @@ entertainment  = ["nature walk", "shopping spree", "city tour", "beach visit"]
 intro = input("Hello and Welcome to your trip planner! Lets get you started!")
 print(intro)
 
-full_trip = f"You'll be visiting {destination} via {transportation}. During your trip, you should eat at a {restuarant} and go on a {entertainment} while you're out!"
+trip_dest = random.choice(destination)
+trip_rest = random.choice(restuarant)
+trip_trans = random.choice(transportation)
+trip_enter = random.choice(entertainment)
+
+
+full_trip = f"You'll be visiting {trip_dest} via {trip_trans}. During your trip, you should eat at a {trip_rest} and go on a {trip_enter} while you're out!"
 print(full_trip)
