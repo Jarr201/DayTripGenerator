bd786676fb8b764a9e676fe25ecd844a7e7006ce
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-08 14:16:13 -0400

    Converted intro input into string, added a satisfaction inquiry input, and placed roadwork for a function using if conditionals.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 
-destination = ["Bahamas", "Tokyo", "New York", "Florida"]
+destination = ["the Bahamas", "Tokyo", "New York", "Florida"]
 restuarant = ["fast food place", "buffet", "diner", "bar"]
 transportation = ["train", "airplane", "car", "cruise ship"]
 entertainment  = ["nature walk", "shopping spree", "city tour", "beach visit"]
 
-intro = input("Hello and Welcome to your trip planner! Lets get you started!")
+intro = "Hello and Welcome to your trip planner! Lets get you started!"
 print(intro)
 
 trip_dest = random.choice(destination)
@@ -16,3 +16,13 @@ trip_enter = random.choice(entertainment)
 
 full_trip = f"You'll be visiting {trip_dest} via {trip_trans}. During your trip, you should eat at a {trip_rest} and go on a {trip_enter} while you're out!"
 print(full_trip)
+
+inquiry = input("Do you like your trip? Y/N ")
+
+if inquiry == "Y":
+    print("We're glad you're satisfied! Have a great trip!")
+elif inquiry == "N":
+    print("No problem! Let's make this plan work for you.")
+else:
+    print(input("Sorry. Please answer again in the correct format. Y/N "))
+    
